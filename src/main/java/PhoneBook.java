772f62e2172cd3b19f78e12806538918527b880c
Ivan Ilin <ilyin1997@icloud.com>
2023-09-03 21:32:19 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> book = new HashMap<>();
+    private int freq;
+
     public int add(String name, String number){
-        return 0;
+        book.put(name,number);
+        return freq+=1;
     }
 }
